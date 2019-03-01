d36afc8baee53b2856e8bfaf98e37ae08e602d28
Oleg Sokolov <falcon@MacBook-Pro-Oleg.local>
2019-03-01 14:13:22 +0200

    consistently fixed

--- sendLargeNumberOfRequests/main.js
+++ sendLargeNumberOfRequests/main.js
@@ -9,17 +9,25 @@ function sendLargeNumberOfRequests(
   url,
   parallelRequests
 ) {
-  for (let i = 0; i < numberOfRequests / parallelRequests; i++) {
-    const promises = [];
-    for (let i = 0; i < parallelRequests; i++) {
-      const promise = fetch(`${url}${id}`).then(resp => resp.text());
-      id++;
-      promises.push(promise);
-    }
-    Promise.all(promises).then(texts => {
-      console.log(texts);
-    });
-  }
+    let requestsSended = 0;
+    (function sendParallelRequests() {
+      const promises = [];
+      for (let i = 0; i < parallelRequests; i++) {
+        const promise = axios.get(`${url}${id}`);
+        id++;
+        requestsSended++;
+        promises.push(promise);
+      }
+      
+      Promise.all(promises).then(texts => {
+        console.log(texts);
+        if (requestsSended < numberOfRequests) {
+          setTimeout(() => {
+            sendParallelRequests();
+          }, 3000);
+        }
+      });
+    })();
 }
 
 sendLargeNumberOfRequests(startId, numberOfRequests, url, parallelRequests);
